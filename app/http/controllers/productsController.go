5b86507924c464805ea03ae4bcfeb09f5d4ede0a
feedlyy <Milktank47>
2020-03-30 17:39:20 +0700

    create method added

--- app/http/controllers/productsController.go
+++ app/http/controllers/productsController.go
@@ -14,6 +14,7 @@ func Products(w http.ResponseWriter, r *http.Request) {
 	var response models.Response
 
 	db := database.Connect()
+	defer db.Close()
 
 	rows, err := db.Query("SELECT * from products")
 	if err != nil {
@@ -27,6 +28,44 @@ func Products(w http.ResponseWriter, r *http.Request) {
 			arr_products = append(arr_products, products)
 		}
 	}
+
+	response.Message = "Success"
+	response.Data = arr_products
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(response)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var products models.Products
+	var arr_products []models.Products
+	var response models.Response
+
+	item := r.FormValue("item")
+	quantity := r.FormValue("quantity")
+
+	db := database.Connect()
+
+	rows, err := db.Exec("insert into products(item, quantity) values (?, ?)", item, quantity)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lastId, _ := rows.LastInsertId()
+
+	getLast, err := db.Query("select * from products where id = ?", lastId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for getLast.Next() {
+		if err := getLast.Scan(&products.Id, &products.Item, &products.Quantity); err != nil {
+			log.Fatal(err.Error())
+		} else {
+			arr_products = append(arr_products, products)
+		}
+	}
+	defer db.Close()
 
 	response.Message = "Success"
 	response.Data = arr_products
