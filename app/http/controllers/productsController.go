2e1281da6b71052839156733a524f48b449a4207
feedlyy <Milktank47>
2020-03-31 12:17:34 +0700

    show method added

--- app/http/controllers/productsController.go
+++ app/http/controllers/productsController.go
@@ -4,11 +4,12 @@ import (
 	"basic_go/app/models"
 	"basic_go/database"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-func Products(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request) {
 	var products models.Products
 	var arr_products []models.Products
 	var response models.Response
@@ -31,6 +32,44 @@ func Products(w http.ResponseWriter, r *http.Request) {
 
 	response.Message = "Success"
 	response.Data = arr_products
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(response)
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	var products models.Products
+	var arr_products []models.Products
+	var response models.Response
+
+	//get the id from url
+	params := mux.Vars(r)
+	Id := params["id"]
+
+	db := database.Connect()
+
+	rows, err := db.Query("select * from products where id = ?", Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(&products.Id, &products.Item, &products.Quantity); err != nil {
+			log.Fatal(err.Error())
+		} else {
+			arr_products = append(arr_products, products)
+		}
+	}
+
+	//cek datanya
+	if arr_products == nil {
+		response.Message = "Data tidak ditemukan"
+		response.Data = nil
+		w.WriteHeader(404)
+	} else {
+		response.Message = "Success"
+		response.Data = arr_products
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(response)
